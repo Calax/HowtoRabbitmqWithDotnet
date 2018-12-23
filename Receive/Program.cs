f50adb17e7d722b2324cc3517718fbf900f4eae6
Alexander <CaxaYou@yandex.ru>
2018-12-23 18:57:16 +0300

    custom acknowledgement

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -14,11 +14,13 @@ namespace Receive
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(queue: "hello",
-                                         durable: false,
+                    channel.QueueDeclare(queue: "task_queue",
+                                         durable: false, //persist to disk.
                                          exclusive: false,
                                          autoDelete: false,
                                          arguments: null);
+                    
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (model, ea) =>
@@ -26,9 +28,11 @@ namespace Receive
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
                         Console.WriteLine(" [x] Received {0}", message);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                     };
-                    channel.BasicConsume(queue: "hello",
-                                         autoAck: true,
+
+                    channel.BasicConsume(queue: "task_queue",
+                                         autoAck: false,
                                          consumer: consumer);
 
                     Console.WriteLine(" Press [enter] to exit.");
--- Send/Program.cs
+++ Send/Program.cs
@@ -8,25 +8,31 @@ namespace Send
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory(){HostName = "localhost"};
+            var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
-                using(var channel = connection.CreateModel())
+                using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(queue: "hello",
+                    channel.QueueDeclare(queue: "task_queue",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
-                    string message = "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
 
-                    channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
-                                 basicProperties: null,
-                                 body: body);
-                    Console.WriteLine(" [x] Sent {0}", message);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        string message = i.ToString();
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
                 }
             }
 
